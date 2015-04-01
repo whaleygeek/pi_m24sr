39dc19f18b5517473e46a9f13d19c62888c620e5
David Whale <david@thinkingbinaries.com>
2015-04-01 08:20:02 +0100

    Simplified crc checker so it only implements ITU-V.41

--- c_proto/crc.c
+++ c_proto/crc.c
@@ -1,79 +1,60 @@
-/* based on http://codepad.org/vf3r1EmJ */
+/* crc.c  01/04/2015  D.J.Whale
+ *
+ * Implements ITU-V.41 FCS algorithm.
+ */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#define CRC_A 1
-#define CRC_B 2
-#define BYTE unsigned char
 
-unsigned short UpdateCrc(unsigned char ch, unsigned short *lpwCrc)
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+
+WORD UpdateCrc(BYTE ch, WORD *pwCrc)
 {
-  ch = (ch^(unsigned char)((*lpwCrc) & 0x00FF));
+  ch = (ch^(BYTE)((*pwCrc) & 0x00FF));
   ch = (ch^(ch<<4));
-  *lpwCrc = (*lpwCrc >> 8) ^ ((unsigned short)ch << 8) ^ ((unsigned short)ch<<3) ^ ((unsigned short)ch>>4);
-  return(*lpwCrc);
+  *pwCrc = (*pwCrc >> 8) 
+         ^ ((WORD)ch << 8) 
+         ^ ((WORD)ch<<3) 
+         ^ ((WORD)ch>>4);
+  return *pwCrc;
 }
 
-void ComputeCrc(int CRCType, char *Data, int Length,
-BYTE *TransmitFirst, BYTE *TransmitSecond)
+WORD ComputeCrc(BYTE *data, unsigned int len, BYTE *crc0, BYTE *crc1)
 {
-  unsigned char chBlock;
-  unsigned short wCrc;
-  switch(CRCType)
-  {
-    case CRC_A:
-      wCrc = 0x6363; /* ITU-V.41 */
-    break;
+  BYTE bBlock;
+  WORD wCrc;
 
-    case CRC_B:
-      wCrc = 0xFFFF; /* ISO/IEC 13239 (formerly ISO/IEC 3309) */
-    break;
-  }
+  wCrc = 0x6363;
 
   do
   {
-    chBlock = *Data++;
-    UpdateCrc(chBlock, &wCrc);
+    bBlock = *data++;
+    UpdateCrc(bBlock, &wCrc);
   }
-  while (--Length);
+  while (--len);
 
-  if (CRCType == CRC_B)
-  {
-    wCrc = ~wCrc; /* ISO/IEC 13239 (formerly ISO/IEC 3309) */
-  }
-  *TransmitFirst = (BYTE) (wCrc & 0xFF);
-  *TransmitSecond = (BYTE) ((wCrc >> 8) & 0xFF);
+  *crc0 = (BYTE) (wCrc & 0xFF);
+  *crc1 = (BYTE) ((wCrc >> 8) & 0xFF);
+  return wCrc;
 }
 
-BYTE BuffCRC_A[10] = {0xC2};
-BYTE BuffCRC_B[10] = {0x0A, 0x12, 0x34, 0x56};
-unsigned short Crc;
-BYTE First, Second;
-FILE *OutFd;
-int i;
 
 int main(void)
 {
-  printf("CRC-16 reference results ISO/IEC 14443-3\n");
-  printf("Crc-16 G(x) = x^16 + x^12 + x^5 + 1\n\n");
-  printf("CRC_A of [ ");
-  for(i=0; i<1; i++)
+  unsigned int i;
+  BYTE crc0, crc1;
+  BYTE buf[] = {0xC2};
+
+  printf("buf:");
+
+  for(i=0; i<sizeof(buf); i++)
   {
-    printf("%02X ",BuffCRC_A[i]);
+    printf("%02X ",buf[i]);
   }
-  ComputeCrc(CRC_A, BuffCRC_A, 1, &First, &Second);
 
-  printf("] Transmitted: %02X then %02X.\n", First, Second);
-  printf("CRC_B of [ ");
+  //TODO return CRC as result
+  ComputeCrc(buf, sizeof(buf), &crc0, &crc1);
 
-  for(i=0; i<4; i++)
-  {
-    printf("%02X ",BuffCRC_B[i]);
-  }
-  ComputeCrc(CRC_B, BuffCRC_B, 4, &First, &Second);
-
-  printf("] Transmitted: %02X then %02X.\n", First, Second);
+  printf("\nCRC:%02X %02X\n", (unsigned int)crc0, (unsigned int)crc1);
   return 0;
 }
